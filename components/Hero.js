8bc20c3c088ad73275049dc43aac1c487fb540e3
Stackbit Code Editor <projects@stackbit.com>
2022-10-21 17:16:31 +0000

    Hero.js: changed

--- components/Hero.js
+++ components/Hero.js
@@ -38,6 +38,7 @@ const Hero = ({
             <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
               <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
               </h1>
+                MLBB FAN TOKEN <strong>From And For FAN</strong>
               <p className="text-black-500 mt-4 mb-6">
                 Provide a network for all your needs with ease and fun using
                 LaslesVPN discover interesting features from us.
