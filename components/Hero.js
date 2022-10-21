931918bb2f8da3fcd546d2b6c51426a09c65ff29
Stackbit Code Editor <projects@stackbit.com>
2022-10-21 17:15:46 +0000

    Hero.js: changed

--- components/Hero.js
+++ components/Hero.js
@@ -37,7 +37,7 @@ const Hero = ({
             variants={scrollAnimation}>
             <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
               <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-                MLBB FAN TOKEN <strong>From And For FAN</strong>.
+                MLBB FAN TOKEN <strong>From And For FAN</strong>
               </h1>
               <p className="text-black-500 mt-4 mb-6">
                 Provide a network for all your needs with ease and fun using
